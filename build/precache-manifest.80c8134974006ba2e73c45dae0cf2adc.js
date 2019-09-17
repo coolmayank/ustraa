12b954a938b8d79c7b427a3a9a3040c17d909137
bhavesh.kumar-1mg <bhavesh.kumar@1mg.com>
2019-09-17 22:47:10 +0530

    Hosted on github pages

---
+++ build/precache-manifest.80c8134974006ba2e73c45dae0cf2adc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a4bfa55d89c1b9e6ea959a16688270",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45cd8160d02bdaf7403c",
+    "url": "/static/css/main.1d697514.chunk.css"
+  },
+  {
+    "revision": "8146c6920509cb4c860f",
+    "url": "/static/js/2.63fa7d2e.chunk.js"
+  },
+  {
+    "revision": "45cd8160d02bdaf7403c",
+    "url": "/static/js/main.137f675e.chunk.js"
+  },
+  {
+    "revision": "f1031339a8e1cc77c42d",
+    "url": "/static/js/runtime~main.81347508.js"
+  }
+]);
